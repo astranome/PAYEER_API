7f83b18fd6266625fcdb3e9041f72aacc50a3a87
astranome <36866164+astranome@users.noreply.github.com>
2022-03-17 02:46:32 +0200

    Add files via upload

---
+++ ord.py
@@ -0,0 +1,27 @@
+"""
+---ASTRA RESEARCH---
+
+API client for PAYEER trading
+Example "orders , ask & bid"
+"""
+
+import requests
+
+import time
+import json
+
+pair = "LTC_USD"
+
+ts = int(round(time.time()*1000))
+ts=str(ts)
+print(ts)
+
+values =json.dumps ({"pair": pair})
+
+
+headers = {'Content-Type': 'application/json'}
+print(headers)
+res = requests.request(method='POST', url='https://payeer.com/api/trade/orders', data=values, headers=headers)
+print(res.text)
+
+
